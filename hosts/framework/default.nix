0fb0ada467d1e4ba16425be1d02b4b0ff06ae9e5
Taha YASSINE <yassinetaha1997@gmail.com>
2024-12-06 10:44:40 +0100

    Enable PPD

--- hosts/framework/default.nix
+++ hosts/framework/default.nix
@@ -14,4 +14,8 @@
 
   # Enable fingerprint reader
   services.fprintd.enable = true;
+
+  # AMD has better battery life with PPD over TLP:
+  # https://community.frame.work/t/responded-amd-7040-sleep-states/38101/13
+  services.power-profiles-daemon.enable = true;
 }
