b51273ba19fff848756b23736d4535cb62c436e2
Taha YASSINE <yassinetaha1997@gmail.com>
2024-12-07 03:44:36 +0100

    Add delta

--- hosts/default/git.nix
+++ hosts/default/git.nix
@@ -7,5 +7,19 @@
 
   programs.lazygit = {
     enable = true;
+    settings = {
+      git.paging = {
+        colorArg = "always";
+        pager = "delta --dark --paging=never --line-numbers --hyperlinks --hyperlinks-file-link-format=\"lazygit-edit://{path}:{line}\"";
+      };
+    };
+  };
+
+  programs.git.delta = {
+    enable = true;
+    options = {
+      side-by-side = true;
+      hyperlinks = true;
+    };
   };
 }
