0d71258340c4283990b1485ed870f0caabe48259
Taha YASSINE <yassinetaha1997@gmail.com>
2025-06-01 07:27:21 +0100

    Set up binary cache

--- hosts/default/default.nix
+++ hosts/default/default.nix
@@ -60,6 +60,13 @@
       experimental-features = "nix-command flakes";
       # Deduplicate and optimize nix store
       auto-optimise-store = true;
+
+      substituters = [
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
     };
 
     gc = {
