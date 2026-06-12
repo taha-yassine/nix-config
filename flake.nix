c41c5315e348221d60eeba19eda5f093c352ca17
Taha YASSINE <yassinetaha1997@gmail.com>
2026-06-12 18:57:39 +0200

    Update to 26.05

--- flake.nix
+++ flake.nix
@@ -9,12 +9,12 @@
     };
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-staging.url = "github:nixos/nixpkgs/staging";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
